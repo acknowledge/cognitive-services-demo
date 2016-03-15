5ff78937d7c898dcda637965443dafd4cd7159cb
acknowledge <jack_ett8@hotmail.com>
2016-03-15 14:49:02 +0100

    add code

---
+++ src/demo/Main.java
@@ -0,0 +1,45 @@
+package demo;
+
+import oxford.api.Emotion;
+import oxford.api.Face;
+
+public class Main {
+
+	final private String EmotionAPIKey = "{write your key here}";
+	final private String FaceAPIKey = "{write your key here}";
+	
+	public static void main(String[] args) {
+		System.out.println("================ START ================");
+		
+		Main process = new Main();
+		process.detectEmotion();
+		process.detectFace();
+		
+		//process.followEmotion();
+		
+		System.out.println("================= END =================");
+	}
+	
+	public void detectEmotion() {
+		String emotion;
+		//emotion = Emotion.runEmotion(EmotionAPIKey, "assets/out6.png");
+		emotion = Emotion.runEmotion(EmotionAPIKey, "http://asylamba.com/public/media/files/sources/out6.png");
+		System.out.println(emotion);
+	}
+	
+	public void detectFace() {
+		String face;
+		//face = Face.runFace(FaceAPIKey, "assets/out6.png");
+		//face = Face.runFace(FaceAPIKey, "http://i.f1g.fr/media/figaro/1280x580_crop/2015/09/09/XVM1bf9bc3e-56f3-11e5-8f47-f6275d19194b.jpg");
+		face = Face.runFace(FaceAPIKey, "http://anotherwhiskyformisterbukowski.com/wp-content/uploads/2014/08/WW.jpg");
+		System.out.println(face);
+	}
+
+	public void followEmotion() {
+		String emotion;
+		for (int i = 0; i < 15; i++) {
+			emotion = Face.runFace(FaceAPIKey, "assets/video1/out" + (i+1) + ".png");
+			System.out.println("Image out" + (i+1) + ".png : " + emotion);
+		}
+	}
+}
---
+++ src/oxford/api/Face.java
@@ -0,0 +1,76 @@
+package oxford.api;
+
+import java.io.File;
+import java.net.URI;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.FileEntity;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+public class Face {
+	
+	final static int REMOTE_IMAGE = 0;
+	final static int LOCAL_IMAGE = 1;
+	
+	static public String runFace(String key, String image) {
+		
+		String JSONResponse = "...";
+		
+		int mode = Emotion.LOCAL_IMAGE;
+		if (image.toLowerCase().contains("http")) {
+			mode = Emotion.REMOTE_IMAGE;
+		}
+		
+		HttpClient httpclient = HttpClients.createDefault();
+
+        try
+        {
+            URIBuilder builder = new URIBuilder("https://api.projectoxford.ai/face/v1.0/detect");
+
+            builder.setParameter("returnFaceId", "true");
+            builder.setParameter("returnFaceLandmarks", "false");
+            builder.setParameter("returnFaceAttributes", "age,gender,smile");
+
+            URI uri = builder.build();
+            HttpPost request = new HttpPost(uri);
+
+            if (mode == Emotion.REMOTE_IMAGE) {
+            	request.setHeader("Content-Type", "application/json");
+            } else {
+            	request.setHeader("Content-Type", "application/octet-stream");
+            }
+            request.setHeader("Ocp-Apim-Subscription-Key", key);
+
+
+            // Request body
+            if (mode == Emotion.REMOTE_IMAGE) {
+            	StringEntity reqEntity = new StringEntity("{ \"url\": \"" + image + "\" }");
+            	request.setEntity(reqEntity);
+            } else {
+            	File file = new File(image);
+            	@SuppressWarnings("deprecation")
+            	FileEntity reqEntity = new FileEntity(file, "binary/octet-stream");
+            	request.setEntity(reqEntity);
+            }
+
+            HttpResponse response = httpclient.execute(request);
+            HttpEntity entity = response.getEntity();
+
+            if (entity != null) 
+            {
+                JSONResponse = EntityUtils.toString(entity);
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+        return JSONResponse;
+	}
+}
